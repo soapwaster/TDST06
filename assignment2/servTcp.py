05767b132ae91e3c1a5093c35a204a52f5bccda6
Valerio Colitta <valco263@parlomba4.ida.liu.se>
2018-09-13 16:16:52 +0200

    add reuseaddr and url checking,

--- assignment2/servTcp.py
+++ assignment2/servTcp.py
@@ -1,76 +1,30 @@
-#import socket
-#import sys
-
-#HOST = ''
-#PORT = int (sys.argv[1])
-#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#s.bind((HOST,PORT))
-
-#s.listen(1)
-#conn, addr = s.accept()
-#while 1 :
-#	data = conn.recv(1024)
-#	if not data : break
-#	conn.sendall("Received %s" %(data))
-#conn.close()
-#s.close()
-
-###################################################################
-
-#import socket
-#import signal
-#import os
-
-#def do_echo(self) :
-#	s.close()
-#	while 1:
-#		data = conn.recv(1024)
-#		if not data : break
-#		conn.sendall(data)
-#	conn.close()
-#	os._exit(0)
-
-#HOST = ''
-#PORT = 5000
-#signal.signal(signal.SIGCHLD, signal.SIG_IGN); #zombie mngmt
-#s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-#s.bind((HOST,PORT))
-#s.listen(1)
-#while 1:
-#	conn, addr = s.accept()
-#	pid = os.fork()
-#	if pid == 0 :
-#		do_echo()
-#	else :
-#		conn.close()
-#	s.close()
-
-###################################################################
-
 import socket
-import struct 
+import struct
 import sys
-import time 
+import time
 import os
 import signal
-from eliza import analyze
 
-HOST = ''
-PORT = int(sys.argv[1])            # The same port as used by the server
+HOST = '127.0.0.1'
+PORT = int(2890)            # The same port as used by the server
 
 def child():
 	s.close()
 	while 1:
-		time.sleep(3)
+
 		data = conn.recv(1024)
-		
 		if not data:break
-		conn.sendall(analyze(data))
+		url_stuff = data.split("\n")[0]
+		if "prova" in url_stuff:
+			print("not good")
+		
+
 	conn.close()
 	os._exit(0)
 
 signal.signal(signal.SIGCHLD,signal.SIG_IGN)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((HOST,PORT))
 s.listen(1)
 while 1:
@@ -81,4 +35,3 @@ while 1:
 	else:
 		conn.close()
 s.close()
-		
