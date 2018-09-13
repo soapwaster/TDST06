907e57ae65d3e25a2670e44fea286629b6da37a1
Theo Foray <theo.foray@etu.unice.fr>
2018-09-13 18:05:54 +0200

    Windows vers update with try to get all chunks

--- assignment2/servTcpWindows.py
+++ assignment2/servTcpWindows.py
@@ -10,15 +10,26 @@ PORT = int(2890)            # The same port as used by the server
 
 def child():
 	s.close()
-	while 1:
-
-		data = conn.recv(1024)
-		if not data:break
-		url_stuff = data.split("\n")[0]
-		if "prova" in url_stuff:
-			print("not good")
-			redirect = "HTTP/1.1 301 Moved Permanently\nLocation: https://www.liu.se//\nConnection: close\nContent-length: 0\r\n\r\n"
-			conn.send(redirect)
+	data = conn.recv(1024)
+	#if not data:break
+	url_stuff = data.split("\n")[0]
+	if "prova" in url_stuff:
+		print("not good")
+		redirect = "HTTP/1.1 301 Moved Permanently\nLocation: https://www.liu.se//\nConnection: close\nContent-length: 0\r\n\r\n"
+		conn.send(redirect)
+	else:
+	#Create socket as client
+		client_req = data
+		total_data = []
+		ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		ss.connect(('www.google.com', 80))
+		ss.send("GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n")
+		while 1:
+			data  = ss.recv(1024)
+			if not data:break
+			
+			total_data.append(data)
+		print(total_data)
 
 
 	conn.close()
