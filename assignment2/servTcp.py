08ec39dbb4790445ac0c566a4c14624ed346c60b
Valerio Colitta <valco263@parlomba3.ida.liu.se>
2018-09-14 10:48:02 +0200

    add xx

--- assignment2/servTcp.py
+++ assignment2/servTcp.py
@@ -15,6 +15,7 @@ def child():
 	data = conn.recv(4096)
 #	if not data:break
 	url_stuff = data.split("\n")[0]
+	print(data)
 	if "prova" in url_stuff:
 		print("not good	")
 		redirect = "HTTP/1.1 301 Moved Permanently\nLocation: https://www.ida.liu.se/~TDTS04/labs/2011/ass2/error1.html//\nConnection: close\nContent-length: 0\r\n\r\n"
@@ -22,18 +23,29 @@ def child():
 	else:
 		#Create socket as client
 		client_req = data
+		total_data = []
 		ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		ss.connect(('www.google.com', 80))
-		ss.send("GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n")
-		data  = ss.recv(1024)
-		print(data)
+		ss.connect(('zebroid.ida.liu.se', 80))
+		ss.send("GET /SpongeBob.html HTTP/1.1\r\nHost: zebroid.ida.liu.se\r\n\r\n")
+		#data  = ss.recv(1024)
+		while 1:
+			data  = ss.recv(1024)
+			#print(data)
+			#print(len(data))
+			if len(data) == 0 :
+				print("OUT")
+				break
+			total_data.append(data)
+	    #print(total_data)
 
-
+		ss.close()
 	conn.close()
 	os._exit(0)
 
 def get_header(http_content):
 	return http_content.split("\r\n")
+
+
 signal.signal(signal.SIGCHLD,signal.SIG_IGN)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
