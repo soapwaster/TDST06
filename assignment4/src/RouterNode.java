faa02d535fcda057b825f0890aa1c9e0aaba0172
Valerio Colitta <valco263@parlomba1.ida.liu.se>
2018-10-17 10:29:41 +0200

    add pr

--- assignment4/src/RouterNode.java
+++ assignment4/src/RouterNode.java
@@ -10,6 +10,8 @@ public class RouterNode {
   private int[] costs = new int[RouterSimulator.NUM_NODES];
   private List<Integer> neighbors = new ArrayList<>();
   private int[][] nTables;
+  private int[] route = new int[RouterSimulator.NUM_NODES];
+  private boolean PoisonReverse = true;
 
   //--------------------------------------------------
   public RouterNode(int ID, RouterSimulator sim, int[] costs) {
@@ -20,6 +22,7 @@ public class RouterNode {
     getNeighbors();
     nTables = new int[RouterSimulator.NUM_NODES][RouterSimulator.NUM_NODES];
     for(int i=0;i<nTables.length;i++) Arrays.fill(nTables[i], RouterSimulator.INFINITY);
+    Arrays.fill(route, -1);
     nTables[myID] = costs;
     //System.out.println(Arrays.toString(nTables[myID]));
     broadcast();
@@ -32,39 +35,21 @@ public class RouterNode {
     
     for(int i = 0; i<pkt.mincost.length; i++){
     	if (i != myID) {
-    		List<Integer> min = new ArrayList<>();
+    		List<Integer> min = new ArrayList<>(RouterSimulator.NUM_NODES);
+    		min.addAll(Collections.nCopies(RouterSimulator.NUM_NODES, RouterSimulator.INFINITY));
         	for(int x : neighbors) {
-        		min.add(costs[x]+nTables[x][i]);
+        		min.add(x, costs[x]+nTables[x][i]);
         	}
         	int min_value = Collections.min(min);
-        	/*if(myID == 2){
-        		System.out.println("min_value : " + min_value);
-            	System.out.println("nTables[" + myID + "]["+i+"]  : " + nTables[myID][i]);
-        	}*/
-        	System.out.println("min_value : "+min_value);
         	if(min_value != nTables[myID][i]){
         		flag = true;
         	}
         	nTables[myID][i] = min_value;
+        	route[i] = min.indexOf(min_value);
+        	System.out.println("To get to "+i+", I use "+route[i]);
     	}
     }
-    
-    /*for(int i=0; i<pkt.mincost.length;i++){
-    	int min = RouterSimulator.INFINITY;
-    	int tmp = nTables[myID][i];
-    	System.out.println(tmp);
-    	 for (int x : neighbors) {
-    		 if(costs[x] + nTables[x][i] < min){
-    			 min = costs[x] + pkt.mincost[i];
-    			 nTables[myID][i] = min;
-    		 }
-    	 }
-    	 System.out.println(nTables[myID][i]);
-    	 if(tmp != nTables[myID][i]) flag = true;
-    }*/
     if(flag) broadcast();
-    
-    //System.out.println("myID = "+ myID +" ----> "+Arrays.toString(nTables[myID]));
   }
 
   //--------------------------------------------------
@@ -91,23 +76,31 @@ public class RouterNode {
 		  }
 		  myGUI.println();
 	  }
-	  myGUI.println();
+	  myGUI.println(".......................");
+	  myGUI.println(Arrays.toString(nTables[myID]));
 	  
   }
 
   //--------------------------------------------------
   public void updateLinkCost(int dest, int newcost) {
-	  /*System.out.println("("+myID+","+dest+") ---> "+newcost);
+	  
 	  costs[dest] = newcost;
-	  int[] fakeCost = costs.clone();
-	  fakeCost[dest] = RouterSimulator.INFINITY;
-	  for (int x : neighbors) {
-			  sendUpdate(new RouterPacket(myID, x, fakeCost));
-	  }
-	  */
-	  costs[dest] = newcost;
-	  nTables[myID][dest] = newcost;
+      nTables[myID][dest] = newcost;
 	  broadcast();
+  
+  }
+  
+  private void poisonBroadcast(int dest){
+	  System.out.println("myID : " + myID + "  dest : " + dest);
+	  int[] fakeCost = nTables[myID].clone();
+	  fakeCost[dest] = RouterSimulator.INFINITY;
+	  
+	  for (int x : neighbors){
+		  if (route[x] == dest){
+		      RouterPacket rp = new RouterPacket(myID, x, fakeCost);
+		      sendUpdate(rp);
+		  }
+	    }
   }
 
   private void getNeighbors(){
@@ -121,8 +114,18 @@ public class RouterNode {
 
   private void broadcast() {
 	  for (int x : neighbors){
-      RouterPacket rp = new RouterPacket(myID, x, nTables[myID]);
-      sendUpdate(rp);
+		  if(PoisonReverse){
+			  int[] fakeCost = nTables[myID].clone();
+			  for (int y=0;y<RouterSimulator.NUM_NODES;y++) {
+				  if(route[y] == x && y!=x)
+					  fakeCost[y] = RouterSimulator.INFINITY;
+			  }
+			  RouterPacket rp = new RouterPacket(myID, x, fakeCost);
+			  sendUpdate(rp);
+			  continue;
+		  }
+		  RouterPacket rp = new RouterPacket(myID, x, nTables[myID]);
+		  sendUpdate(rp);
     }
   }
 
